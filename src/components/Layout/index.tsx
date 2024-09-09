e002c879478c2d0355a7256914c610ddb71ddacb
Mohy-amyal <mohamedmohy900@gmail.com>
2024-09-09 20:37:29 +0300

    fix responsive

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,7 +4,7 @@ type LayoutProps = {
 };
 const Layout = ({ children }: LayoutProps) => {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen my-14">
             <Header />
             <main className="flex-grow px-4 py-6 sm:py-8 md:py-10 overflow-y-auto">
                 {children}
